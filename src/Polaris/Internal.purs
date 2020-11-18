ff1bf487cf5e3538f8fad785d07104728cab0bec
Jan Vincent Liwanag <jvliwanag@gmail.com>
2020-11-18 16:12:11 +0800

    Add PropsWithChildren'

--- src/Polaris/Internal.purs
+++ src/Polaris/Internal.purs
@@ -1,5 +1,6 @@
 module Polaris.Internal
-       ( PropsWithChildren
+       ( PropsWithChildren'
+       , PropsWithChildren
        , elem
        , elemWithChildren
        ) where
@@ -12,22 +13,23 @@ import React.Basic.Hooks (JSX, ReactChildren, ReactComponent, element, reactChil
 import Record as Record
 import Untagged.Coercible (class Coercible, coerce)
 
-type PropsWithChildren p = {children :: ReactChildren JSX| p}
+type PropsWithChildren' a p = {children :: ReactChildren a| p}
+type PropsWithChildren p = PropsWithChildren' JSX p
 
 elem :: forall r props. Coercible r {|props} => ReactComponent {|props} -> r -> JSX
 elem rc = element rc <<< coerce
 
 elemWithChildren ::
-  forall r props a.
-  Row.Lacks "children" props =>
-  Coercible r {|props} =>
-  ReactComponent {children :: ReactChildren a|props} ->
+  forall r baseProps a.
+  Row.Lacks "children" baseProps =>
+  Coercible r {|baseProps} =>
+  ReactComponent (PropsWithChildren' a baseProps) ->
   r ->
   Array a ->
   JSX
 elemWithChildren rc r children = element rc props
   where
-    propsWithoutChildren = coerce r :: {|props}
+    baseProps = coerce r :: {|baseProps}
     props = Record.insert (SProxy :: _ "children")
             (reactChildrenFromArray children)
-            propsWithoutChildren
+            baseProps
